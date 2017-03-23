d8234e6eee362183aa785f8cb5002547bd3ccba9
Laura Vingada <laura@vingada.com>
2017-03-23 18:14:18 -0400

    added React and babel react libraries

---
+++ src/client/app.jsx
@@ -0,0 +1,7 @@
+// @flow
+
+import React from 'react'
+
+const App = () => <h1>All yer cats belong to me. Yeah, I like that.</h1>
+
+export default App
---
+++ src/client/index.jsx
@@ -0,0 +1,28 @@
+// @flow
+
+import 'babel-polyfill'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+
+import App from './app'
+import { APP_CONTAINER_SELECTOR } from '../shared/config'
+
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
+
+const wrappApp = AppComponent =>
+  <AppContainer>
+    <AppComponent />
+  </AppContainer>
+
+ReactDOM.render(wrappApp(App), rootEl)
+
+if (module.hot) {
+  // flow-disable-next-line
+  module.hot.accept('./app', () => {
+    // eslint-disable-next-line global-require
+    const nextApp = require('./app').default
+    ReactDOM.render(wrappApp(nextApp), rootEl)
+  })
+}
